4af157ac4e7d6c6dfb2011edfbb251288d33ea8c
MatheusCavalc <matheuscavalcs@gmail.com>
2024-12-04 15:38:07 -0300

    fix form

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -30,9 +30,10 @@ const sendEmail = () => {
             <div class="flex flex-col lg:flex-row justify-center gap-1 lg:gap-5">
                 <div class="relative z-0 w-full mb-5 group">
                     <input type="text" v-model="nome"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#E40001] peer" />
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#E40001] peer"
+                        placeholder=" " required />
                     <label
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#E40001] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#E40001] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                         Nome</label>
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
