0554728047d6ad72618f9c1bc6607217ba72269f
MatheusCavalc <matheuscavalcs@gmail.com>
2024-12-05 16:22:13 -0300

    update input

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -16,6 +16,13 @@ const sendEmail = () => {
 
     window.location.href = mailtoLink;
 };
+
+const formatPhone = () => {
+    telefone.value = telefone.value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3')
+        .slice(0, 15);
+};
 </script>
 
 <template>
@@ -72,11 +79,12 @@ const sendEmail = () => {
                         Bairro</label>
                 </div>
                 <div class="relative z-0 w-full mb-5 group">
-                    <input type="number" v-model="telefone"
+                    <input type="text" v-model="telefone" @input="formatPhone"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#E40001] peer"
                         placeholder=" " required />
                     <label
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#E40001] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Telefone</label>
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-[#E40001] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                        Telefone</label>
                 </div>
             </div>
 
